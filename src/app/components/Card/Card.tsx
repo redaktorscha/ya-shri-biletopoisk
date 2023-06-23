e47fcc3aebdba5746de093e2b65efbb8e7058796
Masha <nasonkina@yandex.ru>
2023-06-23 20:15:14 +0300

    fix styles

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -2,8 +2,12 @@
 import styles from "./Card.module.css";
 
 export const Card = ({children}) => {
+
+// const gapClass = gap === 1.5 ? "gapMed" : "gapSm";
+// const cardStyles = `${gapClass} ${styles.cardWrapper}`;
+
   return (
-    <div className={styles.cardWrapper}>
+    <div className={`${styles.cardWrapper}`}>
       {children}
     </div>
   )
--- src/app/components/Tickets/Tickets.tsx
+++ src/app/components/Tickets/Tickets.tsx
@@ -4,10 +4,13 @@ import styles from "./Tickets.module.css";
 import Image from "next/image";
 import { Card } from "../Card/Card";
 
-
 const IconButton = ({ handleClick, iconHref, isDisabled, buttonClass }) => {
   return (
-    <button disabled={isDisabled} className={`${buttonClass} ${styles.ticketButton}`} onClick={handleClick}>
+    <button
+      disabled={isDisabled}
+      className={`${buttonClass} ${styles.ticketButton}`}
+      onClick={handleClick}
+    >
       <svg className={styles.ticketButtonIcon}>
         <use href={iconHref} />
       </svg>
@@ -15,8 +18,7 @@ const IconButton = ({ handleClick, iconHref, isDisabled, buttonClass }) => {
   );
 };
 
-const Ticket = ({filmName, filmGenre}) => {
-
+const Ticket = ({ filmName, filmGenre }) => {
   const [count, setCount] = useState(0);
 
   const incrementCount = useCallback(
@@ -45,39 +47,39 @@ const Ticket = ({filmName, filmGenre}) => {
 
   return (
     <Card>
-      <div className={styles.ticketPicWrap}>
+      <div className={styles.ticketWrap}>
         <Image
           width={100}
           height={120}
           src="/poster_small.png"
           alt={`постер фильма`}
-          className={styles.ticketPic}
         />
-      </div>
-      <div className={styles.filmDetails}>
-        <h2 className={styles.filmName}>{filmName}</h2>
-        <span className={styles.filmGenre}>{filmGenre}</span>
-      </div>
-      <div className={styles.ticketInteraction}>
-        <IconButton
-          handleClick={decrementCount}
-          iconHref={"#minus-icon"}
-          isDisabled={isDisabledDecrement}
-          buttonClass={buttonClassDecrement}
-        />
-        <span className={styles.ticketCountLabel}>{count}</span>
-        <IconButton
-          handleClick={incrementCount}
-          iconHref={"#plus-icon"}
-          isDisabled={isDisabledIncrement}
-          buttonClass={buttonClassIncrement}
-        />
-        <div className={styles.closeButtonWrapper}>
-          <button className={styles.closeButton}>
-            <svg className={styles.closeButtonIcon}>
-              <use href="#close-icon" />
-            </svg>
-          </button>
+
+        <div className={styles.filmDetails}>
+          <h2 className={styles.filmName}>{filmName}</h2>
+          <span className={styles.filmGenre}>{filmGenre}</span>
+        </div>
+        <div className={styles.ticketInteraction}>
+          <IconButton
+            handleClick={decrementCount}
+            iconHref={"#minus-icon"}
+            isDisabled={isDisabledDecrement}
+            buttonClass={buttonClassDecrement}
+          />
+          <span className={styles.ticketCountLabel}>{count}</span>
+          <IconButton
+            handleClick={incrementCount}
+            iconHref={"#plus-icon"}
+            isDisabled={isDisabledIncrement}
+            buttonClass={buttonClassIncrement}
+          />
+          <div className={styles.closeButtonWrapper}>
+            <button className={styles.closeButton}>
+              <svg className={styles.closeButtonIcon}>
+                <use href="#close-icon" />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </Card>
@@ -85,25 +87,24 @@ const Ticket = ({filmName, filmGenre}) => {
 };
 
 export const Tickets = () => {
-
   const filmDetails = {
     filmName: "Властелин колец: Братство кольца",
     filmGenre: "Фэнтези",
   };
 
-
   const tickets = [
-    filmDetails, filmDetails,filmDetails,filmDetails,filmDetails
+    filmDetails,
+    filmDetails,
+    filmDetails,
+    filmDetails,
+    filmDetails,
   ];
 
   return (
     <div className={styles.ticketsWrapper}>
-      {
-        tickets.map(({filmName, filmGenre}) => (
-          <Ticket filmName={filmName} filmGenre={filmGenre}/>
-        ))
-      }
+      {tickets.map(({ filmName, filmGenre }) => (
+        <Ticket filmName={filmName} filmGenre={filmGenre} />
+      ))}
     </div>
   );
 };
-
